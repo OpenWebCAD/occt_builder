8547c9f4f7f9f2f1b396205eace80ca33e9fb481
Etienne <etienne.rossignon@gadz.org>
2017-01-23 23:05:04 +0100

    Update build_occt.bat

--- build_occt.bat
+++ build_occt.bat
@@ -1,7 +1,21 @@
 ECHO 
+REM -----------------------------------------------------------------
+REM        DOWNLOADING OFFICIAL OCCT 7.1.0 SOURCE
+REM -----------------------------------------------------------------
 CALL curl  -L -o occt7.1.0.tgz "http://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=89aebdea8d6f4d15cfc50e9458cd8e2e25022326;sf=tgz"
 CALL tar -xf occt7.1.0.tgz
 CALL mv occt-89aebde occt-7.1.0
+
+REM -----------------------------------------------------------------
+REM          PATCHING 7.1.0 TO SPEEDUP BUILD
+REM -----------------------------------------------------------------
+CD occt-7.1.0
+CALL patch -p1 < ../add_cotire_to_7.1.0.patch
+CD ..
+
+REM -----------------------------------------------------------------
+REM       GENERATING SOLUTION
+REM -----------------------------------------------------------------
 CALL mkdir build
 CALL cd build
 CALL cmake -INSTALL_DIR:STRING="%PREFIX%" ^
@@ -19,4 +33,7 @@ CALL cmake -INSTALL_DIR:STRING="%PREFIX%" ^
           -DBUILD_MODULE_Visualization:BOOLEAN=OFF ^
           ../occt-7.1.0
 
+REM -----------------------------------------------------------------
+REM       BUILDING SOLUTION
+REM -----------------------------------------------------------------
 msbuild occt.sln
