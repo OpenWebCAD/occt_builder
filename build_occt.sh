43fad5c9e1f843252a2676953fadd0f4818ce345
Jean-Marie Verdun <verdun@splitted-desktop.com>
2017-02-11 13:36:25 +0100

    First try to generate a deb package

--- build_occt.sh
+++ build_occt.sh
@@ -1,4 +1,42 @@
 
+
+function create_deb {
+rm -rf /tmp/$1-$2
+mkdir /tmp/$1-$2
+mkdir /tmp/$1-$2/DEBIAN
+echo "Package:$1" >> /tmp/$1-$2/DEBIAN/control
+echo "Version:$2" >> /tmp/$1-$2/DEBIAN/control
+echo "Section:base" >> /tmp/$1-$2/DEBIAN/control
+echo "Priority:optional" >> /tmp/$1-$2/DEBIAN/control
+echo "Architecture:amd64" >> /tmp/$1-$2/DEBIAN/control
+echo "Depends:"$3 >> /tmp/$1-$2/DEBIAN/control
+echo "Maintainer:verdun@splitted-desktop.com" >> /tmp/$1-$2/DEBIAN/control
+echo "Homepage:http://ruggedpod.qyshare.com" >> /tmp/$1-$2/DEBIAN/control
+echo "Description:TEST PACKAGE" >> /tmp/$1-$2/DEBIAN/control
+file_list=`ls -ltd $(find /usr/bin) | awk '{ print $9}'`
+file_list2=`ls -ltd $(find /usr/share) | awk '{ print $9}'`
+file_list3=`ls -ltd $(find /usr/include) | awk '{ print $9}'`
+file_list4=`ls -ltd $(find /usr/lib) | awk '{ print $9}'`
+for file in $file_list $file_list2 $file_list3 $file_list4
+do
+  new_file=`printf "%q" "$file"`
+  new_file=`echo $new_file | sed 's/\+/\\\\+/g'`
+  is_done=`cat /tmp/stage | grep -E "$new_file"`
+if [ "$is_done" == "" ]
+then
+# The file must be integrated into the new deb
+    cp --parents $file /tmp/$1-$2
+    echo $file >> /tmp/stage
+fi    
+done
+current_dir=`pwd`
+cd /tmp
+dpkg-deb --build $1-$2
+mv $1-$2.deb $1-$2_trusty-amd64-dev.deb
+cd $current_dir
+}
+
+
 curl  -L -o occt7.1.0.tgz "http://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=89aebdea8d6f4d15cfc50e9458cd8e2e25022326;sf=tgz"
 
 tar -xf occt7.1.0.tgz
@@ -12,11 +50,37 @@ cd occt-7.1.0
 patch -p1 < ../add_cotire_to_7.1.0.patch
 cd ..
 
+
+
+# We must prestage existing file to avoid inserting them into the deb package
+# Files are going to be installed there bin  include  lib  share
+
+file_list=`ls -ltd $(find /usr/bin) | awk '{ print $9}'`
+for file in $file_list
+do
+        echo $file >> /tmp/stage
+done
+file_list=`ls -ltd $(find /usr/include) | awk '{ print $9}'`
+for file in $file_list
+do
+        echo $file >> /tmp/stage
+done
+file_list=`ls -ltd $(find /usr/lib) | awk '{ print $9}'`
+for file in $file_list
+do
+        echo $file >> /tmp/stage
+done
+file_list=`ls -ltd $(find /usr/share) | awk '{ print $9}'`
+for file in $file_list
+do
+        echo $file >> /tmp/stage
+done
+
 mkdir build
 cd build
 export CCACHE_SLOPPINESS="pch_defines;time_macros"
 
-cmake -DINSTALL_DIR:STRING="../dist/occt-7.1.0" \
+cmake -DINSTALL_DIR:STRING="/usr" \
           -DCMAKE_SUPPRESS_REGENERATION:BOOL=ON  \
           -DBUILD_SHARED_LIBS:BOOL=OFF \
           -DBUILD_TESTING:BOOLEAN=OFF \
@@ -32,3 +96,7 @@ cmake -DINSTALL_DIR:STRING="../dist/occt-7.1.0" \
           ../occt-7.1.0
 
 make -j 5  | grep -v "Building CXX"
+# Normally this is what we put at FreeCAD but in our cases Visualization is off so we
+# are not dependant of all these lib
+# create_deb OCCT 7.0 "tcl8.5-dev,tk8.5-dev,libcoin80-dev,libglu1-mesa-dev,g++,cmake"
+create_deb OCCT 7.0 ""
