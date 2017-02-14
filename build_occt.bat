63f1aa43fd672cd9ae7fca1e07299212e9ab6d46
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-02-14 08:31:37 +0100

    improve appveyor build

--- build_occt.bat
+++ build_occt.bat
@@ -1,24 +1,38 @@
+
+
+SET OCCT_VER=occt-7.1.0
+SET PLATFORM=win64
+SET ROOTFOLDER=%~dp0
+SET ARCHIVE_FOLDER=%ROOTFOLDER%\dist\%PLATFORM%
+SET DISTFOLDER=%ARCHIVE_FOLDER%\%OCCT_VER%
+SET ARCHIVE=%OCCT_VER%-%PLATFORM%.zip"
+SET FULL_ARCHIVE=%ARCHIVE_FOLDER%\%ARCHIVE%
+
+
 ECHO 
 ECHO -----------------------------------------------------------------
 ECHO        DOWNLOADING OFFICIAL OCCT 7.1.0 SOURCE
 ECHO -----------------------------------------------------------------
-CALL curl  -L -o occt7.1.0.tgz "http://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=89aebdea8d6f4d15cfc50e9458cd8e2e25022326;sf=tgz"
-CALL tar -xf occt7.1.0.tgz
-CALL mv occt-89aebde occt-7.1.0
+CALL curl  -L -o %OCCT_VER%.tgz "http://git.dev.opencascade.org/gitweb/?p=occt.git;a=snapshot;h=89aebdea8d6f4d15cfc50e9458cd8e2e25022326;sf=tgz"
+CALL tar -xf %OCCT_VER%.tgz
+CALL mv occt-89aebde %OCCT_VER%
 
 ECHO -----------------------------------------------------------------
 ECHO          PATCHING 7.1.0 TO SPEEDUP BUILD
 ECHO -----------------------------------------------------------------
-CD occt-7.1.0
+CD %OCCT_VER%
 CALL patch -p1 < ../add_cotire_to_7.1.0.patch
 CD ..
+
+
+MKDIR %DISTFOLDER%
 
 ECHO -----------------------------------------------------------------
 ECHO       GENERATING SOLUTION
 ECHO -----------------------------------------------------------------
 CALL mkdir build
 CALL cd build
-CALL cmake -INSTALL_DIR:STRING="%PREFIX%" ^
+CALL cmake -INSTALL_DIR:STRING="%DISTFOLDER%" ^
           -DCMAKE_SUPPRESS_REGENERATION:BOOL=ON  ^
           -DBUILD_SHARED_LIBS:BOOL=OFF ^
           -DBUILD_TESTING:BOOLEAN=OFF ^
@@ -31,7 +45,7 @@ CALL cmake -INSTALL_DIR:STRING="%PREFIX%" ^
           -DBUILD_MODULE_ModelingAlgorithms:BOOLEAN=ON ^
           -DBUILD_MODULE_ModelingData:BOOLEAN=ON ^
           -DBUILD_MODULE_Visualization:BOOLEAN=OFF ^
-          ../occt-7.1.0
+          ../%OCCT_VER%
 
 ECHO -----------------------------------------------------------------
 ECHO       BUILDING SOLUTION
@@ -41,7 +55,38 @@ REM SET VERBOSITY=minimal
 
 REM msbuild /m oce.sln
 CALL msbuild /m occt.sln /p:Configuration=Release /verbosity:%VERBOSITY% /consoleloggerparameters:Summary;ShowTimestamp
+ECHO ERROR LEVEL = %ERRORLEVEL%
+f NOT '%ERRORLEVEL%'=='0' goto handle_msbuild_error
 
 ECHO -----------------------------------------------------------------
 ECHO       BUILDING SOLUTION   -> DONE !
 ECHO -----------------------------------------------------------------
+
+
+ECHO -----------------------------------------------------------------
+ECHO       INSTALING TO  %DISTFOLDER%
+ECHO -----------------------------------------------------------------
+CALL msbuild /m INSTALL.vcxproj /p:Configuration=Release /verbosity:%VERBOSITY% /consoleloggerparameters:Summary;ShowTimestamp
+ECHO ERROR LEVEL = %ERRORLEVEL%
+f NOT '%ERRORLEVEL%'=='0' goto handle_install_error
+
+ECHO -----------------------------------------------------------------
+ECHO       CREATING ARCHIVE %DISTFOLDER%
+ECHO -----------------------------------------------------------------
+CD %ARCHIVE_FOLDER%
+7z a %ARCHIVE% %OCCT_VER%\
+CD %ROOTFOLDER%
+DIR %FULL_ARCHIVE%
+
+ECHO -----------------------------------------------------------------
+ECHO      DONE
+ECHO -----------------------------------------------------------------
+
+
+
+exit 0
+
+:handle_install_error
+:handle_msbuild_error
+exit 1
+
